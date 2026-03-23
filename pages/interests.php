9adcd989333f28a00aa005af49b639371da27396
Zaytryn <janotik598@gmail.com>
2026-03-23 08:39:41 +0000

    zmena

--- pages/interests.php
+++ pages/interests.php
@@ -56,7 +56,7 @@ if (isset($_GET['msg'])) {
 <ul>
 <?php foreach ($interests as $key => $i): ?>
     <li>
-        <?php echo ($key + 1) . '. ' . htmlspecialchars($i['name']); ?>
+        <?php echo ($key + 1)?>
         <form method="POST" style="display:inline;">
             <input type="hidden" name="id" value="<?php echo $i['id']; ?>">
             <input type="text" name="name" value="<?php echo htmlspecialchars($i['name']); ?>" required>
